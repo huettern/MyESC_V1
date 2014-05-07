d8917b6afaaa97e2d61539099fbdc0527401962f
noah95 <noahhuetter@gmail.com>
2014-05-07 21:31:30 +0200

    Timer Tick

--- MyEscV1.X/src/basetype.h
+++ MyEscV1.X/src/basetype.h
@@ -8,6 +8,7 @@
 #ifndef BASETYPE_H
 #define	BASETYPE_H
 
+#include <xc.h>
 
 /*****************************************************************************
 * typedefs
@@ -21,6 +22,31 @@ typedef int tSWord;		/* Wetebereich	-32768 - 32767*/
 /*****************************************************************************
 * CONFIGURATION BITS
 *****************************************************************************/
+// CONFIG2
+/* External OSC @ 20MHz -> FCY = 10MHz*/
+#pragma config POSCMOD = HS             // Primary Oscillator Select (HS Oscillator mode selected)
+#pragma config ALTI2C = OFF             // Alternate I2C pins (I2C mapped to SDA1/SCL1)
+#pragma config LPOL = ON                // Motor Control PWM Low Side Polarity bit (PWM module low side output pins have active-high output polarity)
+#pragma config IOL1WAY = OFF             // IOLOCK Protection (Allow Only One Re-configuration)
+#pragma config OSCIOFNC = ON            // Primary Oscillator Output Function (OSC2 pin has digital I/O function)
+#pragma config FCKSM = CSDCMD           // Clock Switching and Monitor (Clock switching and Fail-Safe Clock Monitor are disabled)
+#pragma config FNOSC = PRI              // Oscillator Mode (Primary Oscillator (MS, HS, EC))
+#pragma config WDTWIN = WDTWIN25        // Watchdog Window Select (Watchdog Window is 25% of WDT period)
+#pragma config PWMPIN = ON              // Motor Control PWM Module Pin Mode bit (PWM module pins controlled by PORT register at device Reset)
+#pragma config PWMLOCK = ON             // PWM Lock Enable (Certain PWM registers may only be written after key sequence)
+#pragma config IESO = ON                // Internal External Switch Over Mode (Start-up device with FRC, then automatically switch to user-selected oscillator source when ready)
+
+// CONFIG1
+#pragma config WDTPOST = PS32768        // Watchdog Timer Postscaler (1:32,768)
+#pragma config WDTPRE = PR128           // WDT Prescaler (Prescaler ratio of 1:128)
+#pragma config PLLKEN = ON              // PLL Lock Enable (Clock switch to PLL source will wait until the PLL lock signal is valid.)
+#pragma config WINDIS = OFF             // Watchdog Timer Window (Watchdog Timer in Non-Window mode)
+#pragma config FWDTEN = OFF             // Watchdog Timer Enable (Watchdog timer enabled/disabled by user software)
+#pragma config ICS = PGD3               // Comm Channel Select (Communicate on PGEC1/PGED1)
+#pragma config HPOL = ON                // Motor Control PWM High Side Polarity bit (PWM module high side output pins have active-high output polarity)
+#pragma config GWRP = OFF               // General Code Segment Write Protect (Writes to program memory are allowed)
+#pragma config GCP = OFF                // General Segment Code Protection (General Segment Code protect is disabled)
+
 
 
 
--- MyEscV1.X/src/main.c
+++ MyEscV1.X/src/main.c
@@ -18,6 +18,7 @@
 #include <stdlib.h>
 #include <p33FJ32MC102.h>
 #include "basetype.h"
+ #include <libpic30.h>
 
 
 
@@ -26,9 +27,14 @@
 *****************************************************************************/
 static void          fInit           (void);
 
+void __attribute__((__interrupt__, __auto_psv__)) _T1Interrupt(void);
+
 /*****************************************************************************
 * DEFINES/CONSTANTS
 *****************************************************************************/
+#define TIMER_TICK  1000 //in us
+
+
 #define LED_RED PORTBbits.RB4
 #define LED_GRN PORTAbits.RA4
 
@@ -36,7 +42,7 @@ static void          fInit           (void);
 /*****************************************************************************
 * MODULE VARIABLES
 *****************************************************************************/
-
+tByte bState = 0;
 
 /*****************************************************************************
 * MAIN ROUTINE
@@ -49,11 +55,9 @@ int main(int argc, char** argv)
 {
   fInit();
 
-  
   for(;;)
   {
-    LED_RED = 1;
-    LED_GRN = 1;
+    
   }
   return (EXIT_SUCCESS);
 }
@@ -67,9 +71,37 @@ static void fInit (void)
   Description: intializes controller
   ==========================================================================*/
 {
+  RCONbits.SWDTEN = 0;  //disable WDT
   /* digital IOs */
   TRISBbits.TRISB4 = 0; //LED_RED output
   TRISAbits.TRISA4 = 0; //LED_GRN output
 
+  /* Timer 1 for timer tick */
+  TMR1 = 0;
+  PR1 = TIMER_TICK / 0.1;
+  T1CONbits.TCKPS = 0;  //1/1 PS -> 0.1us @ 10MHz FCY
+  T1CONbits.TCS = 0;  //internal clk
+  IEC0bits.T1IE = 1;    // Enable Timer 1 interrupt
+  T1CONbits.TON = 1;  //start timer
 
+}
+
+
+/*****************************************************************************
+* ISR FUNCTIONS
+*****************************************************************************/
+void __attribute__((__interrupt__, __auto_psv__)) _T1Interrupt(void)
+{
+  IFS0bits.T1IF = 0;
+  if(bState == 0)
+  {
+    LED_RED = 0;
+    bState = 1;
+  }
+  else
+  {
+    LED_RED = 1;
+    bState = 0;
+  }
+  
 }
